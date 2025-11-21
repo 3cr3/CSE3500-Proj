116976b08097572b09d1777c3c365434aca70765
br-ngu <bn107364@gmail.com>
2025-11-20 19:32:26 -0500

    chopsticks.py
    Added GameState and the other functions

--- chopsticks.py
+++ chopsticks.py
@@ -3,7 +3,7 @@ class Hand:
         self.num_fingers = num_fingers
     
     def add(self, val):
-        self.num_fingers = self.num_fingers + val if self.num_fingers + val < 5 else 0
+        self.num_fingers = (self.num_fingers + val) if self.num_fingers + val < 5 else 0
 
 class Player:
     def __init__(self, left=None, right=None):
@@ -33,27 +33,40 @@ class Player:
         return True
     
 class GameState:
-
-    def __init__(self, p1, p2, is_p1_turn = True):
+    def __init__(self, p1, p2, is_p1_turn=True):
         self.p1 = p1
         self.p2 = p2
         self.is_p1_turn = is_p1_turn
 
-    def is_terminal(self): '''check if game over'''
-        p1_dead = self.p1.left.num_fingers == 0 and self.p1.right.num_fingers == 0 #p1 dead
-        p2_dead = self.p2.left.num_fingers == 0 and self.p2.right.num_fingers == 0 #p2 dead
+    def is_terminal(self):
+        # Checks if game is over
+        p1_dead = (self.p1.left.num_fingers == 0 and
+                   self.p1.right.num_fingers == 0)
+        p2_dead = (self.p2.left.num_fingers == 0 and
+                   self.p2.right.num_fingers == 0)
         return p1_dead or p2_dead
-    
+
     def winner(self):
-        #1 if p1 wins, -1 if p2 wins, 0 if no winner (yet)
+        """
+        1  -> Player 1 wins
+        -1 -> Player 2 wins
+        0  -> Game not complete
+        """
+        if self.p2.left.num_fingers == 0 and self.p2.right.num_fingers == 0:
+            return 1
         if self.p1.left.num_fingers == 0 and self.p1.right.num_fingers == 0:
             return -1
-        if self.p2.left.num_fingers == 0 and self.p2.right.num_fingers == 0:
-            return -1
         return 0
-    
+
     def copy(self):
-        from chopsticks import Player, Hand
-        p1_copy = Player(Hand(self.p1.left.num_fingers), Hand(self.p1.right.num_fingers))
-        p2_copy = Player(Hand(self.p2.left.num_fingers), Hand(self.p2.right.num_fingers))
-        return GameState(p1_copy, p2_copy, self.is_p1_turn)
+        return GameState(
+            Player(
+                Hand(self.p1.left.num_fingers),
+                Hand(self.p1.right.num_fingers)
+            ),
+            Player(
+                Hand(self.p2.left.num_fingers),
+                Hand(self.p2.right.num_fingers)
+            ),
+            self.is_p1_turn
+        )
